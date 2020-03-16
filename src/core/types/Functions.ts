0e09c0d1ced5047b29055b28ed7aeab8059ebbdc
Giulia Cavalcanti <giulia_cavalcanti@apple.com>
2020-03-16 20:19:26 -0300

    Revert "starting project"
    This reverts commit 5a39e60cbe7ffc574568bf343011771ec150efbb.
    
    Changes to be committed:
    	deleted:    package-lock.json
    	deleted:    package.json
    	deleted:    src/PageInterface.ts
    	deleted:    src/assets/config.scss
    	deleted:    src/assets/css/App.scss
    	deleted:    src/assets/css/_variables.scss
    	deleted:    src/assets/css/components/Navigation.scss
    	deleted:    src/assets/images/nav-logo.png
    	deleted:    src/components/App.tsx
    	deleted:    src/core/common/ObjectUtils.ts
    	deleted:    src/core/components/Behaviour.ts
    	deleted:    src/core/components/Navigation.tsx
    	deleted:    src/core/types/Functions.ts
    	deleted:    src/index.html
    	deleted:    src/index.tsx
    	deleted:    tsconfig.json
    	deleted:    webpack.config.js
    
    //Revert wrong commit

--- src/core/types/Functions.ts
+++
@@ -1,5 +0,0 @@
-export interface Void{() : void};
-export interface Action<T>{(arg  :T) : void};
-export interface Func<T,R>{(arg : T) : R};
-// Use this when you need pass a Component Function State to be update by another component
-export type LinkedState<T = any> = Action<Action<T>>;
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import './assets/config.scss';
-
-ReactDOM.render (
-    <App />,
-      document.getElementById("root-app")
-    );
