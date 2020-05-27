956689dd4ad7e607aabc86a4e14f59d4bd5dd789
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2020-05-26 23:34:13 -0300

    datalayer Entitdades interface

--- src/main/java/com/react/contabil/datalayer/dataobject/Entidade.java
+++ src/main/java/com/react/contabil/datalayer/dataobject/Entidade.java
@@ -2,7 +2,7 @@ package com.react.contabil.datalayer.dataobject;
 
 /**
  * Interface que todas classes data objects implementam para diferencia-las
- * da chave na classe genérica DaoGenerico<
+ * da chave na classe genérica DaoGenerico
  */
 public interface Entidade {
 }
